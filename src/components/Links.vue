9a8e1b2b34cec089b388ab4cb48875a6b92fd5c1
BennnyTech <151649382+bennnytech@users.noreply.github.com>
2024-05-07 23:09:38 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, Compass, Crosshairs, CommentDotsRegular, Cloud, Cloud } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, Compass, Crosshairs, CommentDotsRegular, Cloud, DiceD6 } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -68,7 +68,7 @@ const siteIcon = {
   Crosshairs,
   CommentDotsRegular,
   Cloud,
-  Cloud,
+  DiceD6,
 };
 
 // 链接跳转
